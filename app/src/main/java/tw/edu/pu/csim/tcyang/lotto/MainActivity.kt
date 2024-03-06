56ff956a8d5542e078d4fe0d450eedb7ed23c978
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-03-06 16:51:04 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -2,10 +2,15 @@ package tw.edu.pu.csim.tcyang.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var t: TextView = findViewById(R.id.txv)
+        t.text = "0"
     }
 }
