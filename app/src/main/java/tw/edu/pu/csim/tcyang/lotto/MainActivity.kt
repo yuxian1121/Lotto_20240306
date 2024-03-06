7cd734905c295ca6a2df37f3ff6e8f08d2c742a5
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-03-06 17:54:24 +0800

    加入歸零功能

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -18,8 +18,14 @@ class MainActivity : AppCompatActivity() {
 
     fun happy(v: View){
         var txv: TextView = findViewById(R.id.txv)
-        txv.text = (1..100).random().toString()
+        if (v.id == R.id.btn){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = "0"
+        }
     }
+
 
 
 
